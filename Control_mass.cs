4aad0678d53b3562c4860010681a474ef01a9a60
Anna <A9183307737@gmail.com>
2022-12-26 16:48:17 +0300

    control 1

---
+++ Control_mass.cs
@@ -0,0 +1,19 @@
+﻿// Задан массив. Сформировать из имеющегося массива новый массив со строками, где кол-во символов <=3
+
+Console.WriteLine("Введите строковые значения элементов массива");
+
+string[] words = Console.ReadLine()!.Split(' ');
+
+void CorrectMass(string[] arr)
+{ 
+    for(int i = 0; i <arr.Length; i++)
+       {
+    if(arr[i].Length <= 3)
+    Console.Write($"{arr[i]} ");
+    }
+}
+Console.WriteLine();
+CorrectMass(words);
+Console.WriteLine();
+
+
